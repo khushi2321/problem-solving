f6c74eb3095549f91c9630b2856151e3fd9dfd63
khushi2321 <162738921+khushi2321@users.noreply.github.com>
2024-03-30 00:36:52 +0530

    Add files via upload

---
+++ Tree 1&2/even_odd_level_difference.cpp
@@ -0,0 +1,11 @@
+ class Solution
+   {
+   public:
+   
+    int getLevelDiff(Node *root)
+    {
+      if (root == NULL) 
+        return 0; 
+return root->data - getLevelDiff(root->left) - getLevelDiff(root->right);  
+    }
+};
